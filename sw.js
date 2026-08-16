653929cd4e485eb5af7df3968f42a9bb681bd8f8
mauriziocreate <65018346+mauriziocreate@users.noreply.github.com>
2026-08-16 10:51:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'deadart-v5';
+const CACHE = 'deadart-v6';
 const FILES = ['./', './index.html', './privacy.html', './manifest.json',
   './icons/icon-192.png', './icons/icon-512.png', './icons/icon-512-maskable.png'];
 self.addEventListener('install', e => {
